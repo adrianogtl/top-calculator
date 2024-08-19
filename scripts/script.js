27ba464a54610420215febfa52cf3205266e218e
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-08-19 16:09:17 -0300

    Add logic for base calculations

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+function operate(operator, num1, num2) {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+      result = "ERROR";
+  }
+  return result;
+}
