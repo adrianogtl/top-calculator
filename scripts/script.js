d1a5dbd62a5e86ea223c6190e6b9d3df133278f7
adrianogtl <80999356+adrianogtl@users.noreply.github.com>
2024-09-06 10:10:04 -0300

    Add basic calculations functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,87 @@
+let calculator = {
+  displayValue: "0",
+  operator: null,
+  firstOperand: 0,
+  secondOperand: 0,
+  result: 0,
+};
+
+const updateDisplay = () => (display.textContent = calculator.displayValue);
+
+function handleButtonEvent(event) {
+  const button = event.target;
+  const buttonValue = event.target.value;
+
+  if (button.classList.contains("number")) {
+    if (calculator.displayValue === "0") {
+      calculator.displayValue = buttonValue;
+    } else {
+      calculator.displayValue += buttonValue;
+    }
+    updateDisplay();
+  }
+
+  if (button.classList.contains("operator")) {
+    calculator.operator = buttonValue;
+    calculator.firstOperand = parseInt(calculator.displayValue);
+    calculator.displayValue = "0";
+    updateDisplay();
+  }
+
+  if (button.classList.contains("equalsSign")) {
+    calculator.secondOperand = parseInt(calculator.displayValue);
+    calculator.result = calculate(
+      calculator.operator,
+      calculator.firstOperand,
+      calculator.secondOperand
+    );
+
+    calculator.displayValue = calculator.result;
+    updateDisplay();
+  }
+
+  if (button.classList.contains("clear")) {
+    calculator = {
+      displayValue: "0",
+      operator: null,
+      firstOperand: 0,
+      secondOperand: 0,
+      result: 0,
+    };
+    updateDisplay();
+  }
+}
+
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-function operate(operator, num1, num2) {
+function calculate(operator, operand1, operand2) {
   let result = 0;
   switch (operator) {
     case "+":
-      result = add(num1, num2);
+      result = add(operand1, operand2);
       break;
     case "-":
-      result = subtract(num1, num2);
+      result = subtract(operand1, operand2);
       break;
     case "*":
-      result = multiply(num1, num2);
+      result = multiply(operand1, operand2);
       break;
     case "/":
-      result = divide(num1, num2);
+      result = divide(operand1, operand2);
       break;
     default:
-      result = "ERROR";
+      result = calculator.displayValue;
   }
   return result;
 }
+
+const display = document.querySelector(".display");
+display.textContent = calculator.displayValue;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonEvent);
+});
